f0ad6d87342cb5ac4c2c214cd450da4ea475e882
saumayg <saumay.gupta.cse19@itbhu.ac.in>
2021-11-15 14:20:05 +0530

    servlet path

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
+USE heroku_01022fbe580f378;
+
 -- DATA FOR USER TABLE
+DELETE FROM user;
+
 INSERT INTO user (username, password, first_name, last_name, email, phone)
 VALUES 
 ('abcd','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','abcd','efgh','abcd@gmail.com', '3434332323'),
@@ -6,15 +10,19 @@ VALUES
 ('mnop','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','mnop','qrst','mnop@gmail.com', '6345344432');
 
 -- DATA FOR post TABLE
+DELETE FROM post;
+
 INSERT INTO post (title, description, body, user_id, created_at)
 VALUES
-('title1', 'description1', 'body1', 2, CURRENT_TIMESTAMP()),
-('title2', 'description2', 'body2', 2, CURRENT_TIMESTAMP()),
-('title3', 'description3', 'body3', 3, CURRENT_TIMESTAMP()),
-('title4', 'description4', 'body4', 3, CURRENT_TIMESTAMP()),
-('title5', 'description5', 'body5', 1, CURRENT_TIMESTAMP());
+('title1', 'description1', 'body1', 15, CURRENT_TIMESTAMP()),
+('title2', 'description2', 'body2', 15, CURRENT_TIMESTAMP()),
+('title3', 'description3', 'body3', 25, CURRENT_TIMESTAMP()),
+('title4', 'description4', 'body4', 25, CURRENT_TIMESTAMP()),
+('title5', 'description5', 'body5', 5, CURRENT_TIMESTAMP());
 
 -- DATA FOR ALBUM TABLE
+DELETE FROM album;
+
 INSERT INTO album (name, description, created_at, user_id, post_id)
 VALUES
 ('Album1', 'Description1', CURRENT_TIMESTAMP(), 1, 5),
@@ -29,11 +37,15 @@ VALUES
 ('Album10', 'Description10', CURRENT_TIMESTAMP(), 1, NULL);
 
 -- DATA FOR ROLES
+DELETE FROM role;
+
 INSERT INTO role (name)
 VALUES
 ('ROLE_USER'), ('ROLE_ADMIN');
 
 -- DATA FOR user_role TABLE
+DELETE FROM user_role;
+
 INSERT INTO user_role (user_id,role_id)
 VALUES 
 (1, 1),
@@ -42,6 +54,8 @@ VALUES
 (3, 1);
 
 -- DATA FOR tag TABLE
+DELETE FROM tag;
+
 INSERT INTO tag (name, description, created_at)
 VALUES
 ('tag1', 'description1', CURRENT_TIMESTAMP()),
@@ -49,6 +63,8 @@ VALUES
 ('tag3', 'description3', CURRENT_TIMESTAMP());
 
 -- DATA for post_tag TABLE
+DELETE FROM post_tag;
+
 INSERT INTO post_tag (post_id, tag_id) 
 VALUES
 (1, 1),
@@ -62,6 +78,8 @@ VALUES
 (5, 1);
 
 -- DATA FOR comment TABLE
+DELETE FROM comment;
+
 INSERT INTO comment (body, post_id, user_id, created_at)
 VALUES
 ('bod1', 1, 1, CURRENT_TIMESTAMP()),
